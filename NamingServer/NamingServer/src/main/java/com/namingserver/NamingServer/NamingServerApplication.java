abc6c671107bbd9a143c7b30ac3fd3ca70d4f443
Nilesh Mhaske <nileshmhaske111@gmail.com>
2023-11-16 01:18:18 +0530

    Commit

--- NamingServer/NamingServer/src/main/java/com/namingserver/NamingServer/NamingServerApplication.java
+++ NamingServer/NamingServer/src/main/java/com/namingserver/NamingServer/NamingServerApplication.java
@@ -3,6 +3,8 @@ package com.namingserver.NamingServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@Eureka
 @SpringBootApplication
 public class NamingServerApplication {
 
