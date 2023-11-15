84b9610fe1f0f857e3e483af94f5ebbe0711570f
Nilesh Mhaske <nileshmhaske111@gmail.com>
2023-11-16 01:18:08 +0530

    commit

--- employee-service/employee-service/src/main/java/com/employeaap/EmployeeServiceApplication.java
+++ employee-service/employee-service/src/main/java/com/employeaap/EmployeeServiceApplication.java
@@ -2,8 +2,13 @@ package com.employeaap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
+//@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
--- employee-service/employee-service/src/main/java/com/employeaap/service/EmployeeService.java
+++ employee-service/employee-service/src/main/java/com/employeaap/service/EmployeeService.java
@@ -4,10 +4,11 @@ import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.employeaap.entity.Employee;
+import com.employeaap.feignclient.AddressClient;
 import com.employeaap.repository.EmployeeRepo;
 import com.employeaap.response.AddressResponse;
 import com.employeaap.response.EmployeeResponse;
@@ -20,21 +21,33 @@ public class EmployeeService {
 
 	@Autowired
 	private ModelMapper mapper;
-	
+
+//	communication using webclient
 //	@Autowired
 //	private WebClient webClient;
 
+// Communication using rest template
+//	@Autowired
+//	private RestTemplate restTemplate; 
+
+// Communication using feign client
 	@Autowired
-	private RestTemplate restTemplate; 
+	private AddressClient addressClient;
 
 	public EmployeeResponse getEmployeeById(int id) {
 
 		Optional<Employee> employee = employeeRepo.findById(id);
 		EmployeeResponse employeeResponse = mapper.map(employee, EmployeeResponse.class);
-		
+//	Using Webclient		
 //		AddressResponse addressResponse = webClient.get().uri("/address/"+id).retrieve().bodyToMono(AddressResponse.class).block();
-	AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8081/address-service/address/{id}", AddressResponse.class, id); 
-        employeeResponse.setAddressResponse(addressResponse); 
+
+//	Using RestTemplate
+//		AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8081/address-service/address/{id}", AddressResponse.class, id);
+//		employeeResponse.setAddressResponse(addressResponse); 
+		
+//	Using Feign client
+		ResponseEntity<AddressResponse> addressResponse = addressClient.getAddressByEmployeeId(id);
+		employeeResponse.setAddressResponse(addressResponse.getBody()); 
 		return employeeResponse;
 	}
 }
